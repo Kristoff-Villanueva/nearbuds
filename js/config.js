ae67a80946fe2e4a958ef30f891640a178cf5175
Jacob Fernandez <jacobdaniel.fernandez@gmail.com>
2022-06-02 00:40:04 +0800

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export const API_KEY = 'cPKxJrVLZvImQ3KB4n'
+export const SECRET_KEY =
+  'kvircgK6QRrx6QYsyArDaEGTjxZxr3bhoW8gPoIVmFK7svcPjBUJ2Xpex3bA9H0tVaTRITPJ7JC5oer8q1FP04Rz'
+export const ADMIN_API_KEY = 'yYuTHfQhty6x1creno'
+export const ADMIN_SECRET_KEY =
+  'NbVMJ4qPTvXOgv0GWWIRBxgAJ2CTjdYDFOfwaZGVTFsRCKY6mhoG832vWeZNFXqItv3jpgmUqpkyb3jbqVoLhHJV'
+export const WALLETS_SERVICE_URL = 'https://api.concati.com/wallets'
+export const USERS_SERVICE_URL = 'https://api.concati.com/users'
+export const ADDRESS_SERVICE_URL = 'https://api.concati.com/address'
+export const MAPS_API_KEY = 'AIzaSyCnnrd__DZ8LrhbWs695Nv2iiND_SzCu24'
